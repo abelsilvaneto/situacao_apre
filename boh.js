5e397854ab7007c8fd030031c2e2794a61f92b27
Abel Silva Neto <abel_silva-neto@estudante.sessisenai.org.br>
2025-10-08 07:22:07 -0300

    first commit

---
+++ boh.js
@@ -0,0 +1,25 @@
+function redirecionar(pagina) {
+    switch (pagina){
+        case 0:
+            window.location.href = "index.html";
+            break;
+        case 1:
+            window.location.href = "Fundamentos_bancos.html";
+            break;
+        case 2:
+            window.location.href = "Logica_Computacional.html";
+            break;
+        case 3:
+            window.location.href = "Fundamentos_banco.html";
+            break;
+        case 4:
+            window.location.href = "Fundamentos_Redes.html";
+            break;
+        case 5:
+            window.location.href = "Logica_programa.html";
+            break;
+        case 6:
+            window.location.href = "funda_eletro.html";
+            break;
+    }
+}
