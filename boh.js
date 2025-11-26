97f340a4bea6cf61e51aa2b3566ff5b90d05ce24
abelsilvaneto <abel_silva-neto@estudante.sesisenai.org.br>
2025-11-26 09:51:56 -0300

    MORAL 21

--- boh.js
+++ boh.js
@@ -10,20 +10,22 @@
             // I = V / R
             // R = V / I
 
+            res.innerHTML = ""
+
             if (V === 0 && I > 0 && R > 0) {
                 V = I * R;
-                res.innerHTML = `Tensão calculada: <strong>${V} V</strong>`;
+                res.innerHTML += `Tensão calculada: <strong>${V} V</strong>`;
             }
             else if (I === 0 && V > 0 && R > 0) {
                 I = V / R;
-                res.innerHTML = `Corrente calculada: <strong>${I} A</strong>`;
+                res.innerHTML += `Corrente calculada: <strong>${I} A</strong>`;
             }
             else if (R === 0 && V > 0 && I > 0) {
                 R = V / I;
-                res.innerHTML = `Resistência calculada: <strong>${R} Ω</strong>`;
+                res.innerHTML += `Resistência calculada: <strong>${R} Ω</strong>`;
             }
             else {
-                res.innerHTML = "Preencha exatamente dois valores para calcular o terceiro!";
+                res.innerHTML += "Preencha exatamente dois valores para calcular o terceiro!";
             }
         }
 
@@ -35,36 +37,39 @@
 
         let res = document.getElementById("res");
 
+        res.innerHTML = ""
+
         if (V > 0 && I > 0) {
             let P = V * I;
-            res.innerHTML = `Potência calculada: <strong>${P} W</strong>`;
+            res.innerHTML += `Potência calculada: <strong>${P} W</strong>`;
         }
         else if (V > 0 && R > 0) {
             let P = (V * V) / R;
-            res.innerHTML = `Potência calculada: <strong>${P} W</strong>`;
+            res.innerHTML += `Potência calculada: <strong>${P} W</strong>`;
         }
         else if (I > 0 && R > 0) {
             let P = (I * I) * R;
-            res.innerHTML = `Potência calculada: <strong>${P} W</strong>`;
+            res.innerHTML += `Potência calculada: <strong>${P} W</strong>`;
         }
         else {
-            res.innerHTML = "Preencha dois valores para calcular a potência!";
+            res.innerHTML += "Preencha dois valores para calcular a potência!";
         }
     }
 
     function gravarDados() {
-        let nome = (document.getElementById("nome").value);
-        let email = (document.getElementById("email").value);
-        let idade = (document.getElementById("idade").value);
+        let nome = document.getElementById("nome").value
+        let email = document.getElementById("email").value
+        let idade = document.getElementById("idade").value
         
-        let res = document.getElementById("res");
+        let res = document.getElementById("res")
 
 
         res.innerHTML = ""
         // Verifica se está vazio
         if (nome === "" || email === "" || idade === "") {
             res.innerHTML += "Preencha todos os campos!"
-        }else {        res.innerHTML += `
+        }else {        
+            res.innerHTML += `
             <h3>Dados Gravados:</h3>
             <p><strong>Nome:</strong> ${nome}</p>
             <p><strong>Email:</strong> ${email}</p>
